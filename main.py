55e8bae89f76a619fbaa3f8fed221808bce7d559
yusancky <yusancky@hotmail.com>
2024-01-30 01:50:19 +0800

    Use model on Huggingface

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import csv
-from modelscope.pipelines import pipeline
-from modelscope.utils.constant import Tasks
 import filetype
 import re
 import requests
 from tqdm import tqdm
+from transformers import pipeline, AutoModelWithLMHead, AutoTokenizer
+import warnings
+
+warnings.filterwarnings('ignore')
 
 re1 = re.compile(r'<td style="text-align:left" id="c24">(.*?)</td>', re.DOTALL)
 re2 = re.compile(r'href="\.\./img/sat/([a-zA-Z\-_0-9]*)\.([a-zA-Z]*)"', re.DOTALL)
@@ -17,6 +19,14 @@ descriptions = []
 
 contentPro = ''
 
+print('loading model...')
+
+model = AutoModelWithLMHead.from_pretrained('Helsinki-NLP/opus-mt-en-zh')
+tokenizer = AutoTokenizer.from_pretrained('Helsinki-NLP/opus-mt-en-zh')
+translation = pipeline('translation_xx_to_yy', model = model, tokenizer = tokenizer)
+
+print('loaded model!')
+
 test = 0
 
 with open('data.csv', newline = '', encoding = 'utf-8') as csvfile:
@@ -26,7 +36,7 @@ with open('data.csv', newline = '', encoding = 'utf-8') as csvfile:
             response = requests.get(row['Link_full'], headers = headers)
             cyberdescription = re3.sub(re3n, re1.search(response.text).group(1).replace("<p>", "").replace("</p>", "").replace("<ul>", "").replace("</ul>", "").replace("  <li>", "- ").replace("</li>", "").replace("  ", ""))
             description = cyberdescription.replace("<b>", "*").replace("</b>", "*").replace("<i>", "_").replace("</i>", "_")
-            contentPro += f'== {row["Value"]} \n === 原文 === \n {description} \n === 中文翻译 \n Translationcky \n'
+            contentPro += f'== {row["Value"]} \n === 原文 === \n {description} \n === 中文翻译 \n {translation(description)[0]["translation_text"]} \n'
         except Exception as e:
             print(f'Fetch {row["Mission name"]} error with {e}!', file = open('stare.txt', 'a'))
         images = (re.findall(re2, response.text))
@@ -35,7 +45,7 @@ with open('data.csv', newline = '', encoding = 'utf-8') as csvfile:
             imageContent = requests.get(image_url, headers = headers).content
             try:
                 imageKind = filetype.guess(imageContent)
-                if result and result.extension in ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp', 'ico']:
+                if imageKind and imageKind.extension in ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp', 'ico']:
                     imageKind = imageKind.extension
                     open(f'images/{image[0].replace("*", "").replace("_", "")}.{imageKind}', 'wb').write(imageContent)
                     contentPro += f'#figure(image("images/{image[0].replace("*", "").replace("_", "")}.{imageKind}"), caption: [{image[0].replace("*", "").replace("_", "")}.{imageKind}])\n'
@@ -46,15 +56,6 @@ with open('data.csv', newline = '', encoding = 'utf-8') as csvfile:
         test += 1
         if test > 25:
             break
-
-try:
-    input_sequence = '<SENT_SPLIT>'.join(descriptions)
-    pipeline_ins = pipeline(task = Tasks.translation, model = "damo/nlp_csanmt_translation_en2zh")
-    outputs = pipeline_ins(input = input_sequence)
-    for translation in tqdm(outputs['translation'].split('<SENT_SPLIT>')):
-        contentPro.replace('Translationcky', translation, 1)
-except Exception as e:
-    print(e)
 
 print(descriptions, file = open('contentPro.list', 'w'))
 print(contentPro, file = open('contentPro.typ', 'a'))
