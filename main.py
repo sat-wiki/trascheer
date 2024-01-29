9dc462aea706c31e339dcd76623dcb4a2966c660
yusancky <yusancky@hotmail.com>
2024-01-29 21:41:16 +0800

    Use filetype to get image type

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
+import filetype
 import re
 import requests
 from tqdm import tqdm
@@ -26,13 +27,22 @@ with open('data.csv', newline = '', encoding = 'utf-8') as csvfile:
             cyberdescription = re3.sub(re3n, re1.search(response.text).group(1).replace("<p>", "").replace("</p>", "").replace("<ul>", "").replace("</ul>", "").replace("  <li>", "- ").replace("</li>", "").replace("  ", ""))
             description = cyberdescription.replace("<b>", "*").replace("</b>", "*").replace("<i>", "_").replace("</i>", "_")
             contentPro += f'== {row["Value"]} \n === 原文 === \n {description} \n === 中文翻译 \n Translationcky \n'
-        except:
-            print(f'{row["Mission name"]}', file = open('stare.txt', 'a'))
+        except Exception as e:
+            print(f'Fetch {row["Mission name"]} error with {e}!', file = open('stare.txt', 'a'))
         images = (re.findall(re2, response.text))
         for image in images:
             image_url = f'https://www.nanosats.eu/img/sat/{image[0]}.{image[1]}'
-            open(f'images/{image[0].replace("*", "").replace("_", "")}.{image[1]}', 'wb').write(requests.get(image_url, headers = headers).content)
-            contentPro += f'#figure(image("images/{image[0].replace("*", "").replace("_", "")}.{image[1]}"), caption: [{image[0].replace("*", "").replace("_", "")}.{image[1]}])\n'
+            imageContent = requests.get(image_url, headers = headers).content
+            try:
+                imageKind = filetype.guess(imageContent)
+                if result and result.extension in ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'webp', 'ico']:
+                    imageKind = imageKind.extension
+                    open(f'images/{image[0].replace("*", "").replace("_", "")}.{imageKind}', 'wb').write(imageContent)
+                    contentPro += f'#figure(image("images/{image[0].replace("*", "").replace("_", "")}.{imageKind}"), caption: [{image[0].replace("*", "").replace("_", "")}.{imageKind}])\n'
+                else:
+                    print(f'Unable to insert {image[0]}.{image[1]}!', file = open('stare.txt', 'a'))
+            except Exception as e:
+                print(f'Unable to insert {image[0]}.{image[1]} with error {e}!', file = open('stare.txt', 'a'))
         test += 1
         if test > 25:
             break
