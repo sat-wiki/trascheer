4a3040832735e50ab7ae47a72006b6457e704c11
yusancky <yusancky@hotmail.com>
2024-01-29 09:05:07 +0800

    Fix Nanosat Problems

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
-from modelscope.pipelines import pipeline
-from modelscope.utils.constant import Tasks
+# from modelscope.pipelines import pipeline
+# from modelscope.utils.constant import Tasks
 import re
 import requests
 from tqdm import tqdm
@@ -17,23 +17,31 @@ contentPro = ''
 with open('data.csv', newline = '', encoding = 'utf-8') as csvfile:
     spamreader = csv.DictReader(csvfile)
     for row in tqdm(spamreader):
-        response = requests.get(row['Link_full'], headers = headers)
-        cyberdescription = re1.search(response.text).group(1).replace("      ", "").replace("<p>", "").replace("</p>", "").replace("<ul>", "").replace("</ul>", "").replace("  <li>", "- ").replace("</li>", "")
-        description = cyberdescription.replace("<b>", "*").replace("</b>", "*").replace("<i>", "_").replace("</i>", "_")
-        contentPro += f'== {row["Value"]}\n\n\u0023table([*原文*], [*中文翻译*], [{description}], [Translationcky])\n'
+        try:
+            response = requests.get(row['Link_full'], headers = headers)
+            cyberdescription = re1.search(response.text).group(1).replace("      ", "").replace("<p>", "").replace("</p>", "").replace("<ul>", "").replace("</ul>", "").replace("  <li>", "- ").replace("</li>", "")
+            description = cyberdescription.replace("<b>", "*").replace("</b>", "*").replace("<i>", "_").replace("</i>", "_")
+            contentPro += f'== {row["Value"]}\n\n\u0023table([*原文*], [*中文翻译*], [{description}], [Translationcky])\n'
+        except:
+            print(f'{row["Mission name"]}', file = open('stare.txt', 'a'))
         images = (re.findall(re2, response.text))
         for image in images:
             image_url = f'https://www.nanosats.eu/img/sat/{image[0]}.{image[1]}'
             open(f'images/{image[0]}.{image[1]}', 'wb').write(requests.get(image_url, headers = headers).content)
             contentPro += f'#figure(image("images/{image[0]}.{image[1]}"), caption: [{image[0]}.{image[1]}])\n'
 
-input_sequence = '<SENT_SPLIT>'.join(descriptions)
-pipeline_ins = pipeline(task = Tasks.translation, model = "damo/nlp_csanmt_translation_en2zh")
-outputs = pipeline_ins(input = input_sequence)
+'''
+try:
+    input_sequence = '<SENT_SPLIT>'.join(descriptions)
+    pipeline_ins = pipeline(task = Tasks.translation, model = "damo/nlp_csanmt_translation_en2zh")
+    outputs = pipeline_ins(input = input_sequence)
+    for translation in tqdm(outputs['translation'].split('<SENT_SPLIT>')):
+        contentPro.replace('Translationcky', translation, 1)
+except:
+    pass
+'''
 
-for translation in tqdm(outputs['translation'].split('<SENT_SPLIT>')):
-    contentPro.replace('Translationcky', translation, 1)
+print(descriptions, file = open('contentPro.list', 'w'))
 print(contentPro, file = open('contentPro.typ', 'a'))
-
 wikiDescription = contentPro.replace("*", "'''").replace("_", "''")
 print(wikiDescription, file = open('content.wikitext', 'w'))
